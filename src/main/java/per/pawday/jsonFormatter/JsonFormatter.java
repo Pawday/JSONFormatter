59207bd43933e4942e2c6fe6d870d61703cc89bb
Pawday <pawday@mail.ru>
2019-05-01 14:12:13 +0300

    new overload: inputStream to string, and rename methon

--- src/main/java/per/pawday/jsonFormatter/JsonFormatter.java
+++ src/main/java/per/pawday/jsonFormatter/JsonFormatter.java
@@ -1,11 +1,12 @@
 package per.pawday.jsonFormatter;
 
+import org.omg.IOP.Encoding;
 import per.pawday.jsonFormatter.constants.IndentChars;
 import per.pawday.jsonFormatter.constants.IndentsStyles;
 import per.pawday.jsonFormatter.exceptions.JsonFormatterException;
 
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
+import java.nio.charset.Charset;
 
 public class JsonFormatter
 {
@@ -30,7 +31,7 @@ public class JsonFormatter
         this.indentCount = indentCount;
     }
 
-    public String formattToString(String string)
+    public String formatToString(String string)
     {
         StringReader reader = new StringReader(string);
 
@@ -169,10 +170,126 @@ public class JsonFormatter
         return builder.toString();
     }
 
+    public String formatToString(InputStream stream, Charset encoding) throws IOException
+    {
+        InputStreamReader reader = new InputStreamReader(stream,encoding);
+        boolean inString = false;
+        int currentIndent = 0;
+
+        int by;
+
+        StringBuilder builder = new StringBuilder();
+        short firstCharController = 0;
+        while ((by = reader.read()) != -1)
+        {
+            char c = (char) by;
+
+            if (! inString)
+            {
+
+                switch (c)
+                {
+                    case '{':
+                    case '[':
+                        if (this.indentStyle == IndentsStyles.ONE_TRUE_BRACING_STYLE)
+                        {
+                            if( firstCharController == 0)
+                            {
+                                builder.append(c).append(newLine);
+                            } else builder.append(c).append(newLine);
+                            currentIndent++;
+                            for (int i = 0; i < currentIndent; i++)
+                            {
+                                builder.append(indentChar);
+                            }
+                        }
+                        else if (this.indentStyle == IndentsStyles.ERIC_ALLMANS_STYLE)
+                        {
+                            if( firstCharController != 0)
+                            {
+                                builder.append(newLine);
+
+                                for (int i = 0; i < currentIndent; i++)
+                                {
+                                    builder.append(indentChar);
+                                }
+                            }
+                            builder.append(c).append(System.lineSeparator());
+
+                            currentIndent++;
+
+                            for (int i = 0; i < currentIndent; i++)
+                            {
+                                builder.append(indentChar);
+                            }
+                        }
+                    break;
+
+                    case '}':
+                    case ']':
 
 
+                        builder.append(newLine);
+                        currentIndent--;
+
+                        for (int i = 0; i < currentIndent; i++)
+                        {
+                            builder.append(indentChar);
+                        }
+                        builder.append(c);
+                    break;
 
 
+                    case '"':
+                        builder.append('"');
+                        inString = true;
+                    break;
 
 
+                    case ':':
+                        builder.append(' ').append(':').append(' ');
+                    break;
+
+                    case ',':
+                        builder.append(',').append(newLine);
+                        for (int i = 0; i < currentIndent; i++)
+                        {
+                            builder.append(indentChar);
+                        }
+                    break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                    case '.':
+                        builder.append(c);
+                    break;
+                }
+            }
+            else
+            {
+                switch (c)
+                {
+                    case '"':
+                        builder.append('"');
+                        inString = false;
+                        break;
+
+                    default:
+                        builder.append(c);
+                }
+            }
+            if(firstCharController == 0)
+            {
+                firstCharController++;
+            }
+        }
+        return builder.toString();
+    }
 }
